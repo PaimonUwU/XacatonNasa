c543b9ef2985e11b95a337df5f384974ec31b171
Andrew2115 <a20310050@ucslp.net>
2022-10-01 18:15:53 -0500

    F

---
+++ Assets/BackgrounScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgrounScript : MonoBehaviour
+{
+    [SerializeField] Vector2 scrollVelocity;
+
+    Material material;
+    void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += scrollVelocity * Time.deltaTime;
+    }
+}
