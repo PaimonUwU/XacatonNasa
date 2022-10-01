48026c35f9c5b5ade7ffadd9af48dc81cb7b7e09
PaimonUwU <113477437+PaimonUwU@users.noreply.github.com>
2022-10-01 00:45:41 -0500

    dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,26 +9,36 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
-    public float velocity;
     public float speed;
+
+    private float movX, movY;
+
+    public Transform dashLeft;
+
+    private bool dashActivated;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        dashActivated = false;
     }
 
    
     void Update()
     {
-        float movX = Input.GetAxis("Horizontal");
-        float movY = Input.GetAxis("Vertical");
+        movX = Input.GetAxis("Horizontal");
+        movY = Input.GetAxis("Vertical");
 
-        moveInput = new Vector2(movX, movY);
-        moveInput.Normalize();
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;//new Vector2(movX, movY);
+                                                                                                         //moveInput.Normalize();
 
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-             
+            if (dashActivated != true)
+            {
+                StartCoroutine(DashCooldown());
+            }
         }
     }
 
@@ -36,5 +46,19 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+
+    IEnumerator DashCooldown()
+    {
+        speed = 15.5f;
+        dashActivated = true;
+
+        yield return new WaitForSeconds(0.3f);
+
+        speed = 6.5f;
+
+        yield return new WaitForSeconds(3f);
+
+        dashActivated = false;
     }
 }
